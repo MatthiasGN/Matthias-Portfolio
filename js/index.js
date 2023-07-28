c5d16d75564438840618aacc6b7f5a498ab6bfae
Matthias Giannesini <matthias.giannesini@gmail.com>
2023-07-28 11:25:55 +1000

    First working draft. Let's go

--- js/index.js
+++ js/index.js
@@ -5,8 +5,30 @@ $(function(){
     $('.greeting').typed({
         strings: ["Hey.", "Hi.", "Yo.", "What's up?"],
         typeSpeed: 70,
-        startDelay:600, 
+        startDelay: 600, 
         backDelay: 4000,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: "|"
+    });
+
+    $('.tools').typed({
+        strings: ["Tools"],
+        typeSpeed: 40,
+        startDelay: 200, 
+        backDelay: 3480,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: "|"
+    });
+
+    $('.languages').typed({
+        strings: ["Languages"],
+        typeSpeed: 40,
+        startDelay: 200, 
+        backDelay: 3000,
         loop: true,
         loopCount: false,
         showCursor: true,
